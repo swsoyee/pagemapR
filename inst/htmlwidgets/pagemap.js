ebd24b5779795a4d4efb2a3d5f7eeb59dc794bbf
swsoyee <swsoyee@gmail.com>
2020-08-29 23:06:15 +0900

    feat: pass option to pagemap

--- inst/htmlwidgets/pagemap.js
+++ inst/htmlwidgets/pagemap.js
@@ -15,19 +15,7 @@ HTMLWidgets.widget({
         map.setAttribute("id", x.id);
         map.setAttribute("style", x.style);
         el.appendChild(map);
-        pagemap(document.querySelector(`#${x.id}`), {
-          viewport: null,
-          styles: {
-            'header,footer,section,article': 'rgba(0,0,0,0.08)',
-            'h1,a': 'rgba(0,0,0,0.10)',
-            'h2,h3,h4': 'rgba(0,0,0,0.08)',
-            'p': 'rgba(0,0,0,0.04)',
-          },
-          back: 'rgba(0,0,0,0.02)',
-          view: 'rgba(0,0,0,0.05)',
-          drag: 'rgba(0,0,0,0.10)',
-          interval: null
-        });
+        pagemap(document.querySelector(`#${x.id}`), x.options);
       },
 
       resize: function (width, height) {
