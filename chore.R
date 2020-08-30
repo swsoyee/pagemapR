03887261b433174362d5f702cc080075a2a267cc
swsoyee <swsoyee@gmail.com>
2020-08-30 18:55:23 +0900

    chore: generate github markdown automatically

--- chore.R
+++ chore.R
@@ -1,3 +1,8 @@
 library(rmarkdown)
 render(input = "./vignettes/pagemap.Rmd",
        output_file = "../docs/index.html")
+render(
+  input = "./vignettes/pagemap.Rmd",
+  output_file = "../README.md",
+  output_format = md_document("markdown_github", pandoc_args = "--wrap=preserve")
+)
